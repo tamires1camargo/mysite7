e6c1e1c7407c18300614df55c25066768dd338f7
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-09-25 18:21:59 -0300

    final

--- mysite7/blog/models/post.py
+++ mysite7/blog/models/post.py
@@ -1,23 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Publish')
+)
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=128)
+class Post(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.name
-
-
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    members = models.ManyToManyField(Person, through="Membership")
-
-    def __str__(self):
-        return self.name
-
-
-class Membership(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    date_joined = models.DateField()
-    invite_reason = models.CharField(max_length=64)
